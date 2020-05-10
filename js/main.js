9521a3f70bd2e5251daaf152b519a4eb0caa8d24
amradelata <amradelata@gmail.com>
2020-05-10 02:26:01 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ sideMenuBtn.addEventListener('click', () => {
 sideMenuBtn_close.addEventListener('click', () => {
     sidebar.style.right = '-100%'
 })
+
+
+/* popup*/
+let mypopup = document.getElementById('mypopup');
+function closepopup(){
+	mypopup.style.display = "none";
+}
+	function shwpopup() {
+	setTimeout(function(){ 
+
+		mypopup.style.display = "block";
+
+	 }, 3000);
+	}
+
+	shwpopup() 
+
+	/* popup*/
